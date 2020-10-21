e5b7f8f263e087e4458fe1b2930d9ac1f058b52d
sarfarazalam <alam.cit@gmail.com>
2020-10-21 20:33:11 +0530

    file added

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+// URL
+define('APP_URL', 'http://localhost/mco/');
+define('APP_SURL', 'http://localhost/mco/');
+// Database
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'u933491855_mcotest');
+define('DB_PREFIX', 'acc_');
+
